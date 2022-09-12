7cca54e707c815e1255fd1214024b6571c1b4deb
root <ghasemi.a.ir@gmail.com>
2022-09-12 20:02:20 +0000

    Update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import pathlib
 
 setuptools.setup(
     name="streamlit_js_eval",
-    version="0.0.1",
+    version="0.0.2",
     packages=setuptools.find_packages(),
     include_package_data=True,
     install_requires=["streamlit==1.12.0"],
