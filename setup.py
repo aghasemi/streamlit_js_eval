a88b4735f8e3a9c034ca02de3781430f0c3c0d7b
root <ghasemi.a.ir@gmail.com>
2022-09-12 20:57:29 +0000

    Specify package data with fix

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ component_name = "streamlit_js_eval"
 
 setuptools.setup(
     name=component_name,
-    version="0.0.4",
+    version="0.0.5",
     packages=setuptools.find_packages(),
     include_package_data=True,
-    package_data={'': [f'{component_name}/*']},
+    package_data={'': [f'./{component_name}/*.html']},
     install_requires=["streamlit==1.12.0"],
     keywords=['Python', 'Streamlit', 'JavaScript'],
     python_requires=">=3.6",
