227308a87ee4a67cf1f964ae0922b4f7a9dff758
root <ghasemi.a.ir@gmail.com>
2022-09-12 21:16:23 +0000

    Package data is back

--- setup.py
+++ setup.py
@@ -5,8 +5,9 @@ component_name = "streamlit_js_eval"
 
 setuptools.setup(
     name=component_name,
-    version="0.0.9",
+    version="0.0.10",
     packages=setuptools.find_packages(),
+    include_package_data=True,
     install_requires=["streamlit==1.12.0"],
     keywords=['Python', 'Streamlit', 'JavaScript'],
     python_requires=">=3.8",
