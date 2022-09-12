abaacf521df592fd0fc1922117142d671c408267
root <ghasemi.a.ir@gmail.com>
2022-09-12 20:54:20 +0000

    Specify package data

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 import setuptools
 import pathlib
 
+component_name = "streamlit_js_eval"
+
 setuptools.setup(
-    name="streamlit_js_eval",
-    version="0.0.3",
+    name=component_name,
+    version="0.0.4",
     packages=setuptools.find_packages(),
     include_package_data=True,
+    package_data={'': [f'{component_name}/*']},
     install_requires=["streamlit==1.12.0"],
     keywords=['Python', 'Streamlit', 'JavaScript'],
     python_requires=">=3.6",
