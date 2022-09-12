3e8403ee2961a971872b47709a1629bb1f6831b8
root <ghasemi.a.ir@gmail.com>
2022-09-12 21:27:34 +0000

    Add url

--- setup.py
+++ setup.py
@@ -5,9 +5,10 @@ component_name = "streamlit_js_eval"
 
 setuptools.setup(
     name=component_name,
-    version="0.0.10",
+    version="0.0.11",
     packages=setuptools.find_packages(),
     include_package_data=True,
+    url='https://github.com/aghasemi/streamlit_js_eval',
     install_requires=["streamlit==1.12.0"],
     keywords=['Python', 'Streamlit', 'JavaScript'],
     python_requires=">=3.8",
