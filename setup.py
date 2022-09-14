2e26016ed1ad8119ba02c146643ff2efb3bb2121
root <ghasemi.a.ir@gmail.com>
2022-09-14 05:44:28 +0000

    Update deps

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ import pathlib
 component_name = "streamlit_js_eval"
 
 # See https://docs.streamlit.io/library/components/publish
-
+# rm -rf dist/;python3 setup.py sdist bdist_wheel;twine upload dist/*
 setuptools.setup(
     name=component_name,
-    version="0.1.0",
+    version="0.1.1",
     description="A custom Streamlit component to evaluate arbitrary Javascript expressions.",
     long_description=pathlib.Path('README.md').read_text(),
     long_description_content_type="text/markdown",
@@ -16,7 +16,7 @@ setuptools.setup(
     author='Alireza Ghasemi',
     author_email='ghasemi.a.ir@gmail.com',
     url='https://github.com/aghasemi/streamlit_js_eval',
-    install_requires=["streamlit==1.12.0"],
+    install_requires=["streamlit>=1.0.0"],
     keywords=['Python', 'Streamlit', 'JavaScript'],
     python_requires=">=3.8",
 )
