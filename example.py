834615babe375375c4d1eba21b4e72617c72a578
root <ghasemi.a.ir@gmail.com>
2022-09-16 08:53:54 +0000

    Update example

--- example.py
+++ example.py
@@ -2,13 +2,13 @@ import streamlit as st
 from streamlit_js_eval import streamlit_js_eval, copy_to_clipboard, create_share_link
 import json
 
-st.write(f"User agent is {streamlit_js_eval(js_expressions='window.navigator.userAgent', want_output = True, key = 'UA')}")
+st.write(f"User agent is _{streamlit_js_eval(js_expressions='window.navigator.userAgent', want_output = True, key = 'UA')}_")
 
-st.write(f"Screen width is {streamlit_js_eval(js_expressions='screen.width', want_output = True, key = 'SCR')}")
+st.write(f"Screen width is _{streamlit_js_eval(js_expressions='screen.width', want_output = True, key = 'SCR')}_")
 
-st.write(f"Browser language is {streamlit_js_eval(js_expressions='window.navigator.language', want_output = True, key = 'LANG')}")
+st.write(f"Browser language is _{streamlit_js_eval(js_expressions='window.navigator.language', want_output = True, key = 'LANG')}_")
 
-st.write(f"Page location is { streamlit_js_eval(js_expressions='window.location.origin', want_output = True, key = 'LOC')}")
+st.write(f"Page location is _{ streamlit_js_eval(js_expressions='window.location.origin', want_output = True, key = 'LOC')}_")
 
 # Copying to clipboard only works with a HTTP connection
 
